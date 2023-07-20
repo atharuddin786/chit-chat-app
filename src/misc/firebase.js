0031c7c4b7aa8e02d12461d950f8081a2901e38a
user.name <atharmail12@gmail.com>
2023-07-20 16:23:22 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKA6txswkIzY6s96CBbwYlzWmKDs1SIuI",
+  authDomain: "chit-chat-app-by-athar.firebaseapp.com",
+  databaseURL: "https://chit-chat-app-by-athar-default-rtdb.firebaseio.com",
+  projectId: "chit-chat-app-by-athar",
+  storageBucket: "chit-chat-app-by-athar.appspot.com",
+  messagingSenderId: "789445305787",
+  appId: "1:789445305787:web:d83f73268594c72c18109e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
