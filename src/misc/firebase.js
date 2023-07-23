5146dcaeee0942e9a6a8879e7df4ae4b13d33616
user.name <atharmail12@gmail.com>
2023-07-23 13:33:33 +0530

    added signin page and login with facebook/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyCKA6txswkIzY6s96CBbwYlzWmKDs1SIuI",
   authDomain: "chit-chat-app-by-athar.firebaseapp.com",
@@ -16,3 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,7 +1,100 @@
 import React from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Grid,
+  Panel,
+  Row,
+  Message,
+  toaster,
+} from "rsuite";
+import FacebookOfficialIcon from "@rsuite/icons/legacy/FacebookOfficial";
+import GooglePlusCircleIcon from "@rsuite/icons/legacy/GooglePlusCircle";
+import {
+  signInWithPopup,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { auth, database } from "../misc/firebase";
+import { ref, serverTimestamp, set } from "firebase/database";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const signInWithProvider = async (provider) => {
+    try {
+      const { user } = await signInWithPopup(auth, provider); // Using await directly here to get the user
+
+      if (user.uid !== null) {
+        await set(ref(database, `/profiles/${user.uid}`), {
+          name: user.displayName,
+          email: user.email,
+          createdAt: serverTimestamp(),
+        });
+
+        console.log(user);
+        toaster.push(
+          <Message showIcon type="success">
+            Signin Successfully
+          </Message>
+        );
+      }
+    } catch (error) {
+      // Handle errors
+      console.error("Error signing in:", error.message);
+      toaster.push(
+        <Message showIcon type="error">
+          Error in Signin
+        </Message>
+      );
+    }
+  };
+
+  const onFacebookSignin = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithProvider(provider);
+  };
+
+  const onGoogleSignin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithProvider(provider);
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>progressive chat platform for neophytes</p>
+              </div>
+              <div className="mt-3">
+                <Button
+                  color="blue"
+                  appearance="primary"
+                  startIcon={<FacebookOfficialIcon />}
+                  block
+                  onClick={onFacebookSignin}
+                >
+                  SignIn with Facebook
+                </Button>
+                <Button
+                  color="green"
+                  appearance="primary"
+                  startIcon={<GooglePlusCircleIcon />}
+                  block
+                  onClick={onGoogleSignin}
+                >
+                  SignIn with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Signin;
