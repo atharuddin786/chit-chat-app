a2bb5b9f154ecc8734ef7c1c42fb4ea6cadee241
user.name <atharmail12@gmail.com>
2023-07-26 21:51:49 +0530

    added signOut option and working

--- src/components/Dashboard/DashboardToggle.js
+++ src/components/Dashboard/DashboardToggle.js
@@ -1,16 +1,37 @@
-import React from "react";
-import { Button, Drawer } from "rsuite";
+import React, { useCallback } from "react";
+import { Button, Drawer, Message, toaster } from "rsuite";
 import DashboardIcon from "@rsuite/icons/Dashboard";
 import { useMediaQuery, useModalState } from "../../misc/custom-hooks";
 import Dashboard from ".";
+import { signOut } from "firebase/auth";
+import { auth } from "../../misc/firebase";
 
 const DashboardToggle = () => {
   const { isShow, hide, show } = useModalState();
   const isMobile = useMediaQuery("(max-width: 992px)");
 
   // Conditionally set the size for mobile and non-mobile devices
-  const drawerSize = isMobile ? "xs" : "full";
+  const drawerSize = isMobile ? "xs" : "md";
+  const onSignOut = useCallback(() => {
+    signOut(auth)
+      .then(() => {
+        toaster.push(
+          <Message showIcon type="info">
+            Sign out successfully
+          </Message>
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+        toaster.push(
+          <Message showIcon type="error">
+            {error}
+          </Message>
+        );
+      });
 
+    hide();
+  }, [hide]);
   return (
     <div>
       <Button
@@ -23,7 +44,7 @@ const DashboardToggle = () => {
         Dashboard
       </Button>
       <Drawer size={drawerSize} open={isShow} onClose={hide} placement="left">
-        <Dashboard />
+        <Dashboard SignOut={onSignOut} />
       </Drawer>
     </div>
   );
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Button, Drawer } from "rsuite";
+import { useProfile } from "../../contex/profile.contex";
 
-const Dashboard = () => {
-  return <div>index</div>;
+const Dashboard = ({ SignOut }) => {
+  const { profile } = useProfile();
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+        <Button color="red" appearance="primary" block onClick={SignOut}>
+          Sign Out
+        </Button>
+      </Drawer.Body>
+    </>
+  );
 };
 
 export default Dashboard;
