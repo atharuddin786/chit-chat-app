820522b81b7c64dc804a80e797b0e125f78c2ccb
user.name <atharmail12@gmail.com>
2023-07-27 02:04:13 +0530

    added editable input field for name

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Button, Drawer } from "rsuite";
+import { Button, Divider, Drawer } from "rsuite";
 import { useProfile } from "../../contex/profile.contex";
+import { EditableInput } from "../EditableInput";
 
 const Dashboard = ({ SignOut }) => {
   const { profile } = useProfile();
+  const Save = async (newData) => {
+    console.log(newData);
+  };
   return (
     <>
       <Drawer.Header>
@@ -11,6 +15,13 @@ const Dashboard = ({ SignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="Nickname"
+          initialValue={profile.name}
+          onSave={Save}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
         <Button color="red" appearance="primary" block onClick={SignOut}>
           Sign Out
         </Button>
---
+++ src/components/EditableInput.js
@@ -0,0 +1,63 @@
+import React, { useCallback, useState } from "react";
+import { Button, Input, InputGroup, Message, toaster } from "rsuite";
+import CheckIcon from "@rsuite/icons/Check";
+import CloseOutlineIcon from "@rsuite/icons/CloseOutline";
+import EditIcon from "@rsuite/icons/Edit";
+
+export const EditableInput = ({
+  initialValue,
+  onSave,
+  label = null,
+  placeholder = "write your nickname",
+  emptyMsg = "name cannot be empty",
+  ...inputProps
+}) => {
+  const [input, setInput] = useState(initialValue);
+  const [isEditable, setIsEditable] = useState(false);
+
+  const onInputChange = useCallback((value) => {
+    setInput(value);
+  }, []);
+  const onEditClick = useCallback(() => {
+    setIsEditable((p) => !p);
+  }, []);
+
+  const trimmed = input.trim();
+
+  const onSaveClick = async () => {
+    if (trimmed === "") {
+      toaster.push(
+        <Message showIcon type="error">
+          {emptyMsg}
+        </Message>
+      );
+      return false;
+    }
+    if (trimmed !== initialValue) {
+      await onSave(trimmed);
+    }
+    setIsEditable(false);
+  };
+  return (
+    <div>
+      {label}
+      <InputGroup>
+        <Input
+          {...inputProps}
+          disabled={!isEditable}
+          placeholder={placeholder}
+          onChange={onInputChange}
+          value={input}
+        />
+        <Button onClick={onEditClick}>
+          {isEditable ? <CloseOutlineIcon /> : <EditIcon />}
+        </Button>
+        {isEditable ? (
+          <Button onClick={onSaveClick}>
+            <CheckIcon />
+          </Button>
+        ) : null}
+      </InputGroup>
+    </div>
+  );
+};
